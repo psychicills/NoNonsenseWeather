8ca7a70ffbe7740a59e9e13277973e6e98de6a5b
unknown <ras0404@gmail.com>
2025-04-20 20:56:29 -0400

    more shi

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const app = express();
+
+const port = 3000; //do i change this? supabase? verecel? 
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile('public/home.html', {root: __dirname});
+});
+
+app.post('/', (req, res) => {
+    res.header("Content-Type", "application/json")
+    const output = {
+        "response": "hello"
+    };
+
+    res.send(JSON.stringify(output));
+});
+
+app.listen(port, () => {
+    console.log(`express app lsitening on port ${port}`)
+});
