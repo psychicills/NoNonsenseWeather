6b217a3f88cc23935e57d5c68175c801e2d94052
unknown <ras0404@gmail.com>
2025-04-26 09:47:56 -0400

    random weather thing done on front page

--- public/homejs.js
+++ public/homejs.js
@@ -9,10 +9,23 @@ function callAPI(){
 }
 
 async function randWeather() {
-    const latitude = (Math.random() * 180 - 90).toFixed(6);
-  const longitude = (Math.random() * 360 - 180).toFixed(6);
+    const latitude = (Math.random() * 180 - 90).toFixed(2);
+  const longitude = (Math.random() * 360 - 180).toFixed(2);
   console.log(latitude)
   console.log(longitude)
-   // fetch (`https://api.open-meteo.com/v1/forecast?
-     //   latitude=52.52&longitude=13.41&hourly=temperature_2m`)
+  document.getElementById("loc").textContent = `Location: ${latitude}, ${longitude}`
+   await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`)
+    .then((response) => response.json())
+    .then((weath) =>{
+        //const time = weath.hourly.time[weath.hourly.time.length - 1]
+        //console.log(time)
+        const temp = weath.hourly.temperature_2m[weath.hourly.temperature_2m.length - 1]
+        console.log (temp)
+        const temp2 = (temp * (9/5)) + 32;
+        document.getElementById("temp").textContent = `Latest Temperature: ${temp2.toFixed(2)}° F`
+    })
 }
+
+
+//add location trying this api later
+//https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en
