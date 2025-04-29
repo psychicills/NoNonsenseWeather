31a8c774f139f1eef03c098353ee35bda3011057
Richard Salters <ras0404@gmail.com>
2025-04-28 23:34:39 -0400

    yurr

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ const port = 3000;
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
 //left off at 35:46
 
